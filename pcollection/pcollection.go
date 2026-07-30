535129f43a60e6d07e144b6493840c448b18dc24
Nguyen Hoang <inqnhoang@gmail.com>
2026-07-29 22:22:34 -0700

    feat: parallelDo

--- pcollection/pcollection.go
+++ pcollection/pcollection.go
@@ -9,3 +9,18 @@ type PCollection[T any] struct {
 func NewPCollection[T any](elements []T) *PCollection[T] {
 	return &PCollection[T]{elements: elements}
 }
+
+func (pc *PCollection[T]) addDependency(fn func()) {
+	pc.dependents = append(pc.dependents, fn)
+}
+
+func parallelDo[T, R any](pc *PCollection[T], fn func(T) R) *PCollection[R] {
+	out := &PCollection[R]{}
+	out.elements = make([]R, len(pc.elements))
+	pc.addDependency(func() {
+		for i, v := range pc.elements {
+			out.elements[i] = fn(v)
+		}
+	})
+	return out
+}
