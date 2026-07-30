a17cb810825b81bed1adcd8f6f7ee2fb6ef00054
Nguyen Hoang <inqnhoang@gmail.com>
2026-07-29 22:49:40 -0700

    feat: flatten

--- pcollection/pcollection.go
+++ pcollection/pcollection.go
@@ -15,12 +15,34 @@ func (pc *PCollection[T]) addDependency(fn func()) {
 }
 
 func parallelDo[T, R any](pc *PCollection[T], fn func(T) R) *PCollection[R] {
+	// TODO not currency safe
 	out := &PCollection[R]{}
-	out.elements = make([]R, len(pc.elements))
 	pc.addDependency(func() {
 		for i, v := range pc.elements {
+			if out.elements == nil {
+				out.elements = make([]R, len(pc.elements))
+			}
 			out.elements[i] = fn(v)
 		}
+		out.executed = true
 	})
+	return out
+}
+
+func flatten[T any](sources ...*PCollection[T]) *PCollection[T] {
+	// TODO not currency safe
+	out := &PCollection[T]{}
+
+	remaining := len(sources)
+	for _, src := range sources {
+		src.addDependency(func() {
+			out.elements = append(out.elements, src.elements...)
+			remaining--
+
+			if remaining == 0 {
+				out.executed = true
+			}
+		})
+	}
 	return out
 }
